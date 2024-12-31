8d882c097010266f884d804786330b55073ce5ee
hbrls <shuaizhexu@gmail.com>
2024-12-31 22:13:31 +0800

    A: wagtail@3.0.3, Django@3.2.6

--- appl/appl/settings/base.py
+++ appl/appl/settings/base.py
@@ -149,7 +149,7 @@ WAGTAIL_SITE_NAME = "appl"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://www.wagtailexample.com'
+WAGTAILADMIN_BASE_URL = 'http://www.wagtailexample.com'
 
 
 SETTINGS_EXPORT = [
